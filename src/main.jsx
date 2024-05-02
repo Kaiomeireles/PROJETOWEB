9f9f01a4994bd077b27b82e59dd4bcb2fb332f9c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-02 20:24:20 -0300

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './COMPONENTS/Nav';
+import Footer from './COMPONENTS/Footer';
+import Error from './Routes/Error';
+import Home from './Routes/Home';
+import Login from './Routes/Login';
+import Produtos from './Routes/Produtos';
+import Contato from './Routes/Contato';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+function Main() {
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/produtos" component={Produtos} />
+        <Route path="/contato" component={Contato} />
+        <Route component={Error} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default Main;
