bebeb2df82b2461d6f905d6ba849f07e87388a04
Lucas Pagung <146399530+LucasdeFreitasPagung@users.noreply.github.com>
2024-05-05 20:59:09 -0300

    Update Home.jsx

--- src/assets/routes/Home.jsx
+++ src/assets/routes/Home.jsx
@@ -1,3 +1,20 @@
-export default function Home(){
+export default function Home(){ import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à Loja de Instrumentos Musicais</h1>
+      <h2>Instrumentos em Destaque:</h2>
+      <ul>
+        <li>Guitarra</li>
+        <li>Piano</li>
+        <li>Bateria</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
+
     
 }
