9485cbf4b3132bc2f2b4119aafcb82232f9e0430
Kaio Meireles <145077870+Kaiomeireles@users.noreply.github.com>
2024-05-05 21:08:03 -0300

    Update Error.jsx

--- src/assets/routes/Error.jsx
+++ src/assets/routes/Error.jsx
@@ -1,3 +1,10 @@
-export default function Error(){
-    
+import React from 'react';
+
+export default function Error() {
+  return (
+    <div>
+      <h1>Erro 404</h1>
+      <p>Página não encontrada.</p>
+    </div>
+  );
 }
